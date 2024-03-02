0bf220c5286f50f337b30970babf44e404f2c430
0xc60f <45770865+0xc60f@users.noreply.github.com>
2024-03-01 22:25:27 -0600

    Updated Astro and AstroPaper

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,27 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://0xc60f.github.io",
+  website: "https://0xc60f.github.io", // replace this with your deployed domain
   author: "0xc60f",
-  desc: "The personal blog and portfolio of 0xc60f",
+  desc: "The personal blog and portfolio of 0xc60f.",
   title: "0xc60f",
   ogImage: "bigimage.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
 
 export const LOGO_IMAGE = {
-  enable: true,
+  enable: false,
   svg: true,
   width: 216,
   height: 46,
 };
-
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
