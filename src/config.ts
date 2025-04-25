e29c6f821d6dabb550494c53524a2aa4dae73457
0xc60f <0xc60f@duck.com>
2025-04-25 14:29:24 -0500

    Updated to 5.2.0

--- src/config.ts
+++ src/config.ts
@@ -1,94 +1,22 @@
-import type { Site, SocialObjects } from "./types";
-
-export const SITE: Site = {
-  website: "https://0xc60f.com/", // replace this with your deployed domain
-  author: "0xc60f",
-  profile: "https://0xc60f.com",
-  desc: "The personal blog and portfolio of 0xc60f.",
-  title: "0xc60f",
-  ogImage: "bigimage.jpg",
+export const SITE = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "Sat Naing",
+  profile: "https://satnaing.dev/",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "AstroPaper",
+  ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 3,
+  postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
+  showBackButton: true, // show back button in post detail
   editPost: {
-    url: "https://github.com/0xc60f/0xc60f.github.io/edit/main/src/content/blog",
+    enabled: true,
     text: "Suggest Changes",
-    appendFilePath: true,
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
-};
-
-export const LOCALE = {
+  dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
-
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/0xc60f",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:contact@0xc60f.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: true,
-  },
-  {
-    name: "Twitch",
-    href: "https://twitch.tv/0xc60f",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: true,
-  },
-  {
-    name: "YouTube",
-    href: "https://youtube.com/@0xc60f",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: true,
-  },
-  {
-    name: "Discord",
-    href: "https://discord.com/users/942859618130989087",
-    linkTitle: `${SITE.title} on Discord`,
-    active: true,
-  },
-  {
-    name: "Reddit",
-    href: "https://reddit.com/user/x0xc60f",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: true,
-  },
-  {
-    name: "Mastodon",
-    href: "https://mastodon.social/@0xc60f",
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: true,
-  },
-  {
-    name: "Steam",
-    href: "https://steamcommunity.com/profiles/76561199575889059/",
-    linkTitle: `${SITE.title} on Steam`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://instagram.com/0xc60f",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
-    name: "X",
-    href: "https://twitter.com/0xc60f",
-    linkTitle: `${SITE.title} on X (Twitter)`,
-    active: true,
-  },
-];
